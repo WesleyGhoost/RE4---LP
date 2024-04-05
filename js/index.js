bc73847ddc99e79cad16d5c8dfb5adfdfefc7530
Wesley <wesley.felix12@hotmail.com>
2024-04-05 10:34:26 -0300

    adds and fixes

--- js/index.js
+++ js/index.js
@@ -1,28 +1,40 @@
-window.addEventListener('scroll', function() {
-    let scrollPosition = window.scrollY
-    let aboutElement = document.getElementById('about')
+window.addEventListener('scroll', function () {
+  let scrollPosition = window.scrollY
+  let aboutElement = document.getElementById('about')
 
-    if(scrollPosition >= aboutElement.offsetTop) {
-        document.body.style.backgroundImage = "url('./imgs/body_bg-2.jpg')"
-    } else {
-        document.body.style.backgroundImage = "url('./imgs/body_bg-1.png')"
-    }
+  if (scrollPosition >= aboutElement.offsetTop) {
+    document.body.style.backgroundImage = "url('./imgs/body_bg-2.jpg')"
+  } else {
+    document.body.style.backgroundImage = "url('./imgs/body_bg-1.png')"
+  }
 })
 
-document.addEventListener("DOMContentLoaded", function() {
-    const thumbs = document.querySelectorAll('.character_thumbs div');
-    
-    thumbs.forEach((thumb, index) => {
-      thumb.addEventListener('click', function() {
-        thumbs.forEach(thumb => {
-          thumb.style.filter = 'grayscale(100%)'
-        })
-        
-        thumb.style.filter = 'grayscale(0%)'
+const thumbs = document.querySelectorAll('.character_thumbs div');
+const carousel = document.getElementById('character_info-carousel')
+const carouselItems = carousel.querySelectorAll('.carousel-item')
 
-        document.getElementById('character_info-carousel').classList.add('slide');
-        document.getElementById('character_info-carousel').querySelector('.carousel-item.active').classList.remove('active');
-        document.getElementById('character_info-carousel').querySelectorAll('.carousel-item')[index].classList.add('active');
-      });
-    });
+carousel.addEventListener('slid.bs.carousel', function () {
+  const activeIndex = Array.from(carouselItems).findIndex(item => item.classList.contains('active'));
+
+  thumbs.forEach((thumb, index) => {
+    if (index !== activeIndex) {
+      thumb.style.filter = 'grayscale(100%)';
+    } else {
+      thumb.style.filter = 'grayscale(0%)';
+    }
   });
+});
+
+thumbs.forEach((thumb, index) => {
+  thumb.addEventListener('click', function () {
+    thumbs.forEach(thumb => {
+      thumb.style.filter = 'grayscale(100%)'
+    })
+
+    thumb.style.filter = 'grayscale(0%)'
+
+    carousel.classList.add('slide');
+    carousel.querySelector('.carousel-item.active').classList.remove('active');
+    carouselItems[index].classList.add('active');
+  });
+});
